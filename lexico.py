5a5b479804a6a1fa55757f470e0e7f899d9d23d5
Luis Mario <luis.mario.trujillo@live.com.mx>
2022-11-18 13:11:41 -0600

    Lex and main

---
+++ lexico.py
@@ -0,0 +1,13 @@
+
+# PALABRAS RESERVADAS
+palabras_reservadas = {
+    'program': 'PROGRAM',
+    'int': 'INT',
+    'float': 'FLOAT',
+    'var': 'VARS',
+    'if': 'IF',
+    'print': 'PRINT',
+    'else': 'ELSE'
+}
+
+tokens = ['ID', 'CTEI', 'CTEF', 'CTESTRING']
